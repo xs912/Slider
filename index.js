6855db640d88fc4d48598c4e579e7c92eabf0dfb
xs912 <58208280+xs912@users.noreply.github.com>
2020-02-16 13:22:51 +0400

    Reworked code

--- index.js
+++ index.js
@@ -5,17 +5,25 @@ for (let i = 0; i < controls.length; i++) {
 
 let slides = document.querySelectorAll("#slides .slide");
 let currentSlide = 0;
-let rads = document.getElementsByName("radbut");
 let myform = document.getElementById("myDiv");
 let links = document.getElementsByTagName("a");
+let radArray = document.querySelectorAll(".radio");
+let next = document.getElementById("next");
+let previous = document.getElementById("previous");
+
+next.onclick = function() {
+  nextSlide();
+};
+previous.onclick = function() {
+  previousSlide();
+};
 
 function nextSlide() {
   if (currentSlide === 2) {
     links[2].className = "inactive";
   }
   goToSlide(currentSlide + 1);
-  console.log(currentSlide);
-  rads[currentSlide].checked = true;
+  radArray[currentSlide].checked = true;
   links[currentSlide].className = "active";
   links[currentSlide - 1].className = "inactive";
 }
@@ -31,75 +39,45 @@ function previousSlide() {
     m = currentSlide - 1;
   }
   goToSlide(currentSlide - 1);
-  rads[m].checked = true;
+  radArray[m].checked = true;
   links[m].className = "active";
   links[m + 1].className = "inactive";
 }
-
 function goToSlide(n) {
   slides[currentSlide].className = "slide";
   currentSlide = (n + slides.length) % slides.length;
   slides[currentSlide].className = "slide showing";
 }
 
-let next = document.getElementById("next");
-let previous = document.getElementById("previous");
+for (let i = 0; i < radArray.length; i++) {
+  radArray[i].addEventListener("click", function(event) {
+    goToSlide(i);
+    links[i].className = "active";
+    if (i == 0) {
+      links[i + 2].className = "inactive";
+    } else {
+      links[i - 1].className = "inactive";
+    }
+    if (i == 2) {
+      links[i - 2].className = "inactive";
+    }
+    links[i + 1].className = "inactive";
+  });
+}
 
-next.onclick = function() {
-  nextSlide();
-};
-previous.onclick = function() {
-  previousSlide();
-};
-
-let radio1 = document.getElementById("point1");
-let radio2 = document.getElementById("point2");
-let radio3 = document.getElementById("point3");
-radio1.addEventListener("click", function(event) {
-  goToSlide(1);
-  link1.className = "active";
-  link2.className = "inactive";
-  link3.className = "inactive";
-});
-
-radio2.addEventListener("click", function(event) {
-  goToSlide(2);
-  link2.className = "active";
-  link1.className = "inactive";
-  link3.className = "inactive";
-});
-
-radio3.addEventListener("click", function(event) {
-  goToSlide(3);
-  link3.className = "active";
-  link2.className = "inactive";
-  link1.className = "inactive";
-});
-
-let link1 = document.getElementById("link1");
-let link2 = document.getElementById("link2");
-let link3 = document.getElementById("link3");
-
-link1.addEventListener("click", function(event) {
-  goToSlide(1);
-  this.className = "active";
-  link2.className = "inactive";
-  link3.className = "inactive";
-  myform.point1.checked = true;
-});
-
-link2.addEventListener("click", function(event) {
-  goToSlide(2);
-  this.className = "active";
-  link1.className = "inactive";
-  link3.className = "inactive";
-  myform.point2.checked = true;
-});
-
-link3.addEventListener("click", function(event) {
-  goToSlide(3);
-  this.className = "active";
-  link1.className = "inactive";
-  link2.className = "inactive";
-  myform.point3.checked = true;
-});
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function(event) {
+    goToSlide(i);
+    links[i].className = "active";
+    if (i == 0) {
+      links[i + 2].className = "inactive";
+    } else {
+      links[i - 1].className = "inactive";
+    }
+    if (i == 2) {
+      links[i - 2].className = "inactive";
+    }
+    links[i + 1].className = "inactive";
+    myform.radArray[i].checked = true;
+  });
+}
